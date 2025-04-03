3d54840772dbca49e279b506ba4bec3661f9e3c6
vhermawan <vihermawan7@gmail.com>
2025-04-04 00:15:32 +0700

    feat: change bg color selected date

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,7 +54,7 @@ function Calendar({
         day_range_start: 'day-range-start',
         day_range_end: 'day-range-end',
         day_selected:
-          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+          'bg-blue-500 text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
         day_today: 'bg-accent text-accent-foreground',
         day_outside:
           'day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground',

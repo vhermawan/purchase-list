a4548613f45d962c568f905939d2a04a633c960a
vhermawan <vihermawan7@gmail.com>
2025-04-04 18:10:19 +0700

    chore: update script

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'pnpm dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
